ff51fc2a3ae682e3a94d0b2e94ba859534f9f8d7
Jackson Meires <lordjackson@gmail.com>
2024-08-05 17:31:27 -0300

    up aula insert e listagem

--- sig_acad/paginas/AlunoList.php
+++ sig_acad/paginas/AlunoList.php
@@ -1,6 +1,16 @@
 <?php
     include './aula03_funcao.php';
+    include "../db.class.php";
+
     head();
+
+    $db = new db();
+    $db->conn();
+
+    $dados = $db->all();
+
+    //var_dump($dados);
+   // exit;
 ?>
 
 <div class="col">
@@ -29,28 +39,24 @@
         <tr>
         <th scope="col">#</th>
         <th scope="col">Nome</th>
-        <th scope="col">Email</th>
+        <th scope="col">CPF</th>
         <th scope="col">Telefone</th>
         </tr>
     </thead>
     <tbody>
-        <tr>
-        <th scope="row">1</th>
-        <td>Mark</td>
-        <td>Otto</td>
-        <td>@mdo</td>
-        </tr>
-        <tr>
-        <th scope="row">2</th>
-        <td>Jacob</td>
-        <td>Thornton</td>
-        <td>@fat</td>
-        </tr>
-        <tr>
-        <th scope="row">3</th>
-        <td colspan="2">Larry the Bird</td>
-        <td>@twitter</td>
-        </tr>
+        <?php 
+
+        foreach($dados as $item){
+           // var_dump($item);
+            //exit;
+           echo "<tr>
+                    <th scope='row'>$item->id</th>
+                    <td>$item->nome</td>
+                    <td>$item->cpf</td>
+                    <td>$item->telefone</td>
+                </tr>";
+         }
+        ?>
     </tbody>
     </table>
 </div>
